ae37c2a761ddabae5422efe382048501c0d90645
Вадим <vadimrudenko386@gmail.com>
2025-05-28 14:16:39 +0300

    add

---
+++ banking/Account.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+class Account {
+public:
+    Account(double initial = 0.0);
+
+    void deposit(double amount);
+    void withdraw(double amount);
+    double getBalance() const;
+
+private:
+    double balance;
+};
---
+++ banking/Transaction.hpp
@@ -0,0 +1,9 @@
+
+
+#include "Account.hpp"
+
+class Transaction {
+public:
+    static void transfer(Account& from, Account& to, double amount);
+};
+
